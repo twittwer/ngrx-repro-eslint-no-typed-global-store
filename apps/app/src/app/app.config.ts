b5c50fcb1a938b3941c13fc207555c6324652639
Tobias Wittwer <t.wittwer95@gmail.com>
2023-07-10 11:17:40 +0200

    Add NgRx
    npx nx g @nx/angular:ngrx-root-store --project=app

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [provideEffects(), provideStore()],
 };
